82695968719eca20644da14acaf39fa4043c0d54
Proshanto <proshanto@mpower-social.com>
2017-11-08 19:05:26 +0600

    work on anc

---
+++ opensrp-etl/src/main/java/org/opensrp/etl/util/NumbertUtil.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.opensrp.etl.util;
+
+/**
+ * @author proshanto
+ */
+public class NumbertUtil {
+	
+	public static Integer convertToInteger(String str) {
+		
+		return Integer.parseInt(str);
+		
+	}
+}
